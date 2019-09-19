2bf79cc72855fb697c005ef31ead195aa3d521ad
Daniel Brauer <messagetodaniel@gmail.com>
2019-09-19 23:08:03 +0200

    config typo

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@ module.exports = {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretKey: process.env.AWS_SECRET_KEY,
         region: process.env.AWS_REGION,
-        s3Bucked: process.env.AWS_S3_BUCKET,
+        s3Bucket: process.env.AWS_S3_BUCKET,
     },
     databaseUrl: process.env.DATABASE_URL,
     jwtSecret: process.env.JWT_SECRET,
