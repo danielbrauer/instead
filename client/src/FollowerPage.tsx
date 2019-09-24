74bfdcebabee5924fd07d3ec8f4c895a46be0a22
Daniel Brauer <messagetodaniel@gmail.com>
2019-09-24 23:16:58 +0200

    Follower page styling

--- client/src/FollowerPage.tsx
+++ client/src/FollowerPage.tsx
@@ -1,48 +1,66 @@
 import React from 'react'
 import FollowUserForm from './Components/FollowUserForm'
-import { Button, List, Header } from 'semantic-ui-react'
+import { Button, List, Header, Icon } from 'semantic-ui-react'
 import { FollowRequest, User } from './Interfaces'
 
 export interface FollowerPageProps {
-    requests : FollowRequest[],
-    followers : number[],
-    followees : number[],
-    follow: (username : string) => Promise<any>,
-    accept: (userid : number) => void,
-    reject: (userid : number) => void,
-    unfollow: (userid : number) => void,
-    removeFollower: (userid : number) => void,
-    getUser: (userid : number) => User,
+    requests: FollowRequest[],
+    followers: number[],
+    followees: number[],
+    follow: (username: string) => Promise<any>,
+    accept: (userid: number) => void,
+    reject: (userid: number) => void,
+    unfollow: (userid: number) => void,
+    removeFollower: (userid: number) => void,
+    getUser: (userid: number) => User,
 }
 
-export default function FollowerPage(props : FollowerPageProps) {
+export default function FollowerPage(props: FollowerPageProps) {
     return (
         <div>
             <FollowUserForm callback={props.follow} />
-            <List>
+            <Header>Requests</Header>
+            <List verticalAlign='middle'>
                 {props.requests.map((request) => (
                     <List.Item key={request.requester_id}>
-                        <Button onClick={() => props.accept(request.requester_id)}>accept</Button>
-                        <Button onClick={() => props.reject(request.requester_id)}>reject</Button>
-                        <List.Content>{props.getUser(request.requester_id).username}</List.Content>
+                        <List.Content floated='right'>
+                            <Button.Group size='mini'>
+                                <Button color='green' onClick={() => props.accept(request.requester_id)}>Accept</Button>
+                                <Button color='red' onClick={() => props.reject(request.requester_id)}>Reject</Button>
+                            </Button.Group>
+                        </List.Content>
+                        <List.Content>
+                            <Icon size='big' name='user'/>
+                            {props.getUser(request.requester_id).username}
+                        </List.Content>
                     </List.Item>
                 ))}
             </List>
             <Header>Followers</Header>
-            <List>
+            <List verticalAlign='middle'>
                 {props.followers.map(follower => (
                     <List.Item key={follower}>
-                    <Button onClick={() => props.removeFollower(follower)}>remove</Button>
-                        <List.Content>{props.getUser(follower).username}</List.Content>
+                        <List.Content floated='right'>
+                            <Button color='red' onClick={() => props.removeFollower(follower)}>remove</Button>
+                        </List.Content>
+                        <List.Content>
+                            <Icon size='big' name='user'/>
+                            {props.getUser(follower).username}
+                        </List.Content>
                     </List.Item>
                 ))}
             </List>
             <Header>Following</Header>
-            <List>
+            <List verticalAlign='middle'>
                 {props.followees.map(followee => (
                     <List.Item key={followee}>
-                    <Button onClick={() => props.unfollow(followee)}>unfollow</Button>
-                        <List.Content>{props.getUser(followee).username}</List.Content>
+                        <List.Content floated='right'>
+                            <Button color='red' onClick={() => props.unfollow(followee)}>unfollow</Button>
+                        </List.Content>
+                        <List.Content>
+                            <Icon size='big' name='user'/>
+                            {props.getUser(followee).username}
+                        </List.Content>
                     </List.Item>
                 ))}
             </List>
