9d8e0660835833c1527ad98ad3b5db8f23fe236f
Daniel Brauer <messagetodaniel@gmail.com>
2019-09-25 23:59:39 +0200

    Cleaned up post header a little

--- client/src/Posts.tsx
+++ client/src/Posts.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CurrentUser from './CurrentUser'
-import { Button, List } from 'semantic-ui-react'
+import { List } from 'semantic-ui-react'
 import { Post, User } from './Interfaces'
 import EncryptedImage from './EncryptedImage'
 import SafetyButton from './SafetyButton'
@@ -19,7 +19,12 @@ export default function (props : PostsProps) {
                 {props.posts.map(post => (
                     <List.Item key={post.id}>
                         {CurrentUser.getId() === post.author_id ?
-                            <SafetyButton onClick={() => props.delete(post.id)}>Delete</SafetyButton> : null}
+                            <List.Content floated='right'>
+                                <SafetyButton size='mini' onClick={() => props.delete(post.id)}>Delete</SafetyButton>
+                            </List.Content>
+                            :
+                            null}
+                        <List.Icon name='user' />
                         <List.Content>{props.getUser(post.author_id).username}</List.Content>
                         <EncryptedImage encryptedUrl={props.contentUrl + post.filename} iv={post.iv} decKey={post.key} />
                     </List.Item>
