c3d2b826f40991ba43108965554937a4c0652e44
Daniel Brauer <messagetodaniel@gmail.com>
2019-12-05 17:15:11 +0100

    Added attribution and cleaned up compression script

--- client/compress.js
+++ client/compress.js
@@ -1,37 +1,38 @@
-const fs = require("fs");
-const path = require("path");
-const glob = require("glob");
-const async = require("async");
-const zlib = require('zlib');
+// adapted from https://gist.github.com/heyimalex/db6e734d1c4e0ac50e0dc8f2abeeae4e
 
-function gzipFile(src, callback) {
-  console.log(`Compressing ${src}`);
-  const dst = `${src}.br`;
-  fs.createReadStream(src)
-    .on("error", callback)
-    .pipe(
-      zlib.createBrotliCompress()
-    )
-    .on("error", callback)
-    .pipe(fs.createWriteStream(dst))
-    .on("error", callback)
-    .on("close", () => {
-      const srcSize = fs.statSync(src).size;
-      const dstSize = fs.statSync(dst).size;
-      console.log(`  Saved ${srcSize - dstSize} bytes`);
-      console.log(`  ${Math.floor((100 * dstSize) / srcSize)}% of original`);
-      callback();
-    });
+import { createReadStream, createWriteStream, statSync } from 'fs'
+import { sync } from 'glob'
+import { eachSeries } from 'async'
+import { createBrotliCompress } from 'zlib'
+
+function compressAsBrotli(src, callback) {
+    console.log(`Compressing ${src}`)
+    const dst = `${src}.br`
+    createReadStream(src)
+        .on('error', callback)
+        .pipe(
+            createBrotliCompress()
+        )
+        .on('error', callback)
+        .pipe(createWriteStream(dst))
+        .on('error', callback)
+        .on('close', () => {
+            const srcSize = statSync(src).size;
+            const dstSize = statSync(dst).size;
+            console.log(`  Saved ${srcSize - dstSize} bytes`);
+            console.log(`  ${Math.floor((100 * dstSize) / srcSize)}% of original`);
+            callback()
+        })
 }
 
 function compressBuildArtifacts(callback) {
-  const assets = glob.sync("./build/**/*.@(js|css|html)");
-  async.eachSeries(assets, gzipFile, callback);
+    const assets = sync('./build/**/*.@(js|css|html)')
+    eachSeries(assets, compressAsBrotli, callback)
 }
 
 compressBuildArtifacts(err => {
-  if (err) {
-    throw err;
-  }
-  console.log("Compression completed sucessfully");
-});
+    if (err) {
+        throw err
+    }
+    console.log('Compression completed sucessfully')
+})
