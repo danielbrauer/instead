44ab16b00c2b3d136c1ad4caf2b875442f482b5c
Daniel Brauer <messagetodaniel@gmail.com>
2020-07-09 12:30:21 +0200

    Switched AppAuthenticated to function
    Fixed username display

--- client/src/AppAuthenticated.tsx
+++ client/src/AppAuthenticated.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CurrentUser from './CurrentUser'
 import FollowerPage from './FollowerPage'
 import Posts from './Posts'
@@ -6,55 +6,51 @@ import NewPost from './NewPost'
 import { Route, Switch, Redirect, RouteComponentProps } from 'react-router-dom'
 
 import { Menu, Dropdown } from 'semantic-ui-react'
-import { queryCache } from 'react-query'
-import { logOut } from './RoutesAuthenticated'
+import { queryCache, useQuery } from 'react-query'
+import { logOut, getUser } from './RoutesAuthenticated'
 
-class App extends Component<RouteComponentProps<any>> {
+export default function(props: RouteComponentProps<any>) {
+    if (!CurrentUser.loggedIn())
+        return (<Redirect to='/login' />)
 
-    logOutAndClear = async () => {
+    const currentUserName = useQuery(['user', CurrentUser.getId()], getUser)
+
+    const logOutAndClear = async () => {
         try {
             await logOut()
         } finally {
             queryCache.clear()
             CurrentUser.clear()
-            this.props.history.push('/login')
+            props.history.push('/login')
         }
     }
-
-    render() {
-        if (!CurrentUser.loggedIn())
-            return (<Redirect to='/login' />)
-        // const currentUser = useQuery(['user', CurrentUser.getId()], Routes.getUser)
-        return (
-            <div>
-                <Menu inverted fixed='top' size='small'>
-                    <Menu.Item header>
-                        Instead
-                    </Menu.Item>
-                    <Menu.Item fitted position='right'>
-                        <Dropdown item direction='left' text='{currentUser.data?.username}'>
-                            <Dropdown.Menu>
-                                <Dropdown.Item icon='list' text='Home' onClick={() => this.props.history.push('/home')}/>
-                                <Dropdown.Item icon='image' text='New Post' onClick={() => this.props.history.push('/new')}/>
-                                <Dropdown.Item icon='user' text='Followers' onClick={() => this.props.history.push('/followers')}/>
-                                <Dropdown.Divider />
-                                <Dropdown.Item icon='sign-out' text='Log Out' onClick={this.logOutAndClear}/>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Menu.Item>
-                </Menu>
-                <br />
-                <br />
-                <Switch>
-                    <Route path='/followers' render={props => <FollowerPage {...props}/>} />
-                    <Route path='/following' render={props => <FollowerPage {...props} />} />
-                    <Route path='/requests' render={props => <FollowerPage {...props} />} />
-                    <Route path='/home'><Posts /></Route>
-                    <Route path='/new' render={props => <NewPost {...props} />} />
-                </Switch>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Menu inverted fixed='top' size='small'>
+                <Menu.Item header>
+                    Instead
+                </Menu.Item>
+                <Menu.Item fitted position='right'>
+                    <Dropdown item direction='left' text={currentUserName.isSuccess ? currentUserName.data.username : 'loading...'}>
+                        <Dropdown.Menu>
+                            <Dropdown.Item icon='list' text='Home' onClick={() => props.history.push('/home')}/>
+                            <Dropdown.Item icon='image' text='New Post' onClick={() => props.history.push('/new')}/>
+                            <Dropdown.Item icon='user' text='Followers' onClick={() => props.history.push('/followers')}/>
+                            <Dropdown.Divider />
+                            <Dropdown.Item icon='sign-out' text='Log Out' onClick={logOutAndClear}/>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
+            </Menu>
+            <br />
+            <br />
+            <Switch>
+                <Route path='/followers' render={props => <FollowerPage {...props}/>} />
+                <Route path='/following' render={props => <FollowerPage {...props} />} />
+                <Route path='/requests' render={props => <FollowerPage {...props} />} />
+                <Route path='/home'><Posts /></Route>
+                <Route path='/new' render={props => <NewPost {...props} />} />
+            </Switch>
+        </div>
+    )
 }
-
-export default App
