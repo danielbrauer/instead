59d683c8796179c4337066ec6446933c3baec5f6
Daniel Brauer <messagetodaniel@gmail.com>
2020-07-17 11:04:52 +0200

    Only throw on login mutations

--- client/src/AppUnauthenticated.tsx
+++ client/src/AppUnauthenticated.tsx
@@ -4,20 +4,29 @@ import LoginForm from './Components/LoginForm'
 import SignupForm from './Components/SignupForm'
 import { Grid } from 'semantic-ui-react'
 import { Route, Switch, Redirect } from 'react-router-dom'
+import { ReactQueryConfigProvider } from 'react-query'
+
+const queryConfig = {
+    mutations: {
+        throwOnError: true,
+    },
+}
 
 export default function() {
     if (CurrentUser.loggedIn())
         return (<Redirect to='/home'/>)
 
     return (
-        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-            <Grid.Column style={{ maxWidth: 450 }}>
-                <Switch>
-                    <Route exact path="/" render={props => <Redirect {...props} to="/login" />} />
-                    <Route exact path="/signup" render={props => <SignupForm {...props} />} />
-                    <Route exact path="/login" render={props => <LoginForm {...props} />} />
-                </Switch>
-            </Grid.Column>
-        </Grid>
+        <ReactQueryConfigProvider config={queryConfig}>
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Switch>
+                        <Route exact path="/" render={props => <Redirect {...props} to="/login" />} />
+                        <Route exact path="/signup" render={props => <SignupForm {...props} />} />
+                        <Route exact path="/login" render={props => <LoginForm {...props} />} />
+                    </Switch>
+                </Grid.Column>
+            </Grid>
+        </ReactQueryConfigProvider>
     )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,36 +5,27 @@ import AppAuthenticated from './AppAuthenticated'
 import AppUnauthenticated from './AppUnauthenticated'
 import AppWelcome from './AppWelcome'
 import 'semantic-ui-css/semantic.min.css'
-import { ReactQueryConfigProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query-devtools'
 
-const queryConfig = {
-    mutations: {
-        throwOnError: true,
-    },
-}
-
 const routing = (
-    <ReactQueryConfigProvider config={queryConfig}>
-        <BrowserRouter>
-            <ReactQueryDevtools initialIsOpen={false} />
-            <Switch>
-                <Route exact path="/" component={AppUnauthenticated} />
-                <Route path="/signup" component={AppUnauthenticated} />
-                <Route path="/login" component={AppUnauthenticated} />
+    <BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Switch>
+            <Route exact path="/" component={AppUnauthenticated} />
+            <Route path="/signup" component={AppUnauthenticated} />
+            <Route path="/login" component={AppUnauthenticated} />
 
-                <Route path="/home" component={AppAuthenticated} />
-                <Route path="/user/:username" component={AppAuthenticated} />
-                <Route path="/post/:postid" component={AppAuthenticated} />
-                <Route path="/followers" component={AppAuthenticated} />
-                <Route path="/following" component={AppAuthenticated} />
-                <Route path="/requests" component={AppAuthenticated} />
-                <Route path="/new" component={AppAuthenticated} />
+            <Route path="/home" component={AppAuthenticated} />
+            <Route path="/user/:username" component={AppAuthenticated} />
+            <Route path="/post/:postid" component={AppAuthenticated} />
+            <Route path="/followers" component={AppAuthenticated} />
+            <Route path="/following" component={AppAuthenticated} />
+            <Route path="/requests" component={AppAuthenticated} />
+            <Route path="/new" component={AppAuthenticated} />
 
-                <Route path="/welcome" component={AppWelcome} />
-            </Switch>
-        </BrowserRouter>
-    </ReactQueryConfigProvider>
+            <Route path="/welcome" component={AppWelcome} />
+        </Switch>
+    </BrowserRouter>
 )
 
 ReactDOM.render(routing, document.getElementById('root'))
