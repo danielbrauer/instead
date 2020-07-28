d0e00022dc0e4bcd29f53576a1cf166d07b854f9
Daniel Brauer <messagetodaniel@gmail.com>
2020-07-28 22:24:10 +0200

    DatabaseService tests

---
+++ backend/src/services/DatabaseService.spec.ts
@@ -0,0 +1,57 @@
+import { mocked } from 'ts-jest/utils'
+import DatabaseService from './DatabaseService'
+import pg, { PoolClient } from 'pg'
+
+jest.mock('pg')
+jest.mock('pg-camelcase')
+jest.mock('../config/config')
+
+describe('DatabaseService', () => {
+
+    let databaseService: DatabaseService
+
+    beforeAll(() => {
+        databaseService = new DatabaseService()
+    })
+
+    describe('transaction', () => {
+
+        let dummyClient = {
+            query: jest.fn((x: string) => {}),
+            release: jest.fn(() => {}),
+        }
+
+        beforeEach(async() => {
+            mocked(databaseService.pool).connect.mockImplementation(() => dummyClient)
+        })
+
+        afterEach(() => {
+            jest.resetAllMocks()
+        })
+
+        test('releases on completion', async() => {
+            await databaseService.transaction(async(client) => {
+                client.query('TEST')
+            })
+
+            expect(dummyClient.query.mock.calls).toHaveLength(3)
+            expect(dummyClient.query.mock.calls[0][0]).toEqual('BEGIN')
+            expect(dummyClient.query.mock.calls[1][0]).toEqual('TEST')
+            expect(dummyClient.query.mock.calls[2][0]).toEqual('COMMIT')
+            expect(dummyClient.release.mock.calls).toHaveLength(1)
+        })
+
+        test('rolls back on throw', async() => {
+            await expect(databaseService.transaction((client) => {
+                client.query('TEST')
+                throw new Error()
+            })).rejects.toThrow()
+
+            expect(dummyClient.query.mock.calls).toHaveLength(3)
+            expect(dummyClient.query.mock.calls[0][0]).toEqual('BEGIN')
+            expect(dummyClient.query.mock.calls[1][0]).toEqual('TEST')
+            expect(dummyClient.query.mock.calls[2][0]).toEqual('ROLLBACK')
+            expect(dummyClient.release.mock.calls).toHaveLength(1)
+        })
+    })
+})
