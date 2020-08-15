27c6c57eebff58df432589bc3bed88d8d1d9e875
Daniel Brauer <messagetodaniel@gmail.com>
2020-08-15 13:06:15 +0200

    Applied some rules for test

--- client/src/PostHeader.tsx
+++ client/src/PostHeader.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import CurrentUser from "./CurrentUser";
+import CurrentUser from './CurrentUser'
 import { Post } from '../../backend/src/types/api'
-import { useMutation } from "react-query"
+import { useMutation } from 'react-query'
 import { deletePost } from './RoutesAuthenticated'
-import SafetyButton from "./SafetyButton";
-import UserInList from './UserInList';
+import SafetyButton from './SafetyButton'
+import UserInList from './UserInList'
 
 export interface Props {
     post: Post
 }
 
-export default function PostHeader({ post } : Props) {
+export default function PostHeader({ post }: Props) {
     const [deletePostMutation] = useMutation(deletePost)
 
     return (
         <div>
             <UserInList id={post.authorId} />
-            {CurrentUser.getId() === post.authorId ?
-                <SafetyButton floated='right' size='mini' onClick={() => deletePostMutation(post.id)}>Delete</SafetyButton>
-                :
-                null}
+            {CurrentUser.getId() === post.authorId ? (
+                <SafetyButton floated='right' size='mini' onClick={() => deletePostMutation(post.id)}>
+                    Delete
+                </SafetyButton>
+            ) : null}
         </div>
     )
 }
