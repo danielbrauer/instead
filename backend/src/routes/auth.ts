f9b4a24f6390cc29dd346f7727b90c92249442b9
Daniel Brauer <messagetodaniel@gmail.com>
2020-08-16 15:37:02 +0200

    Add logging on error

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -13,34 +13,42 @@ const authService = Container.get(AuthService)
 
 router.use(delayResponse(config.int('MIN_AUTH_TIME'), config.int('MAX_AUTH_TIME')))
 
-router.post('/startLogin',
+router.post(
+    '/startLogin',
     validator.query(Schema.empty),
     validator.body(Schema.startLoginBody),
     async function (req: ValidatedRequest<Schema.StartLoginRequest>, res) {
-        const responseData = await authService.startLogin(req.session, req.body.username, req.body.clientEphemeralPublic)
+        const responseData = await authService.startLogin(
+            req.session,
+            req.body.username,
+            req.body.clientEphemeralPublic,
+        )
         return res.json(responseData)
-    }
+    },
 )
 
-router.post('/finishLogin',
+router.post(
+    '/finishLogin',
     validator.query(Schema.empty),
     validator.body(Schema.finishLoginBody),
     async function (req: ValidatedRequest<Schema.FinishLoginRequest>, res) {
         const responseData = await authService.finishLogin(req.session, req.body.clientSessionProof)
         return res.json(responseData)
-    }
+    },
 )
 
-router.get('/startSignup',
+router.get(
+    '/startSignup',
     validator.query(Schema.empty),
     validator.body(Schema.empty),
     async function (req: ValidatedRequest<Schema.StartSignupRequest>, res) {
         const responseData = await authService.startSignup(req.session)
         return res.json(responseData)
-    }
+    },
 )
 
-router.post('/finishSignup',
+router.post(
+    '/finishSignup',
     validator.query(Schema.empty),
     validator.body(Schema.finishSignupBody),
     async function (req: ValidatedRequest<Schema.FinishSignupRequest>, res) {
@@ -50,19 +58,19 @@ router.post('/finishSignup',
         }
         const responseData = await authService.finishSignup(req.session, newUser)
         return res.json(responseData)
-    }
+    },
 )
 
-router.put('/cancel',
-    validator.query(Schema.empty),
-    validator.body(Schema.empty),
-    async function (req, res) {
-        delete req.session
-        return res.send('Authentication cancelled')
-    }
-)
+router.put('/cancel', validator.query(Schema.empty), validator.body(Schema.empty), async function (
+    req,
+    res,
+) {
+    delete req.session
+    return res.send('Authentication cancelled')
+})
 
 router.use(function hangupHandler(err: any, req: any, res: any, next: any) {
+    console.log(err)
     delete req.session
     return res.status(403).send('Authentication failed')
 })
