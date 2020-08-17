2725c967965f988a716e16af45bc2beaac2b79e0
Daniel Brauer <messagetodaniel@gmail.com>
2020-08-17 19:47:31 +0200

    Infinite scrolling

--- client/src/Posts.tsx
+++ client/src/Posts.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useInfiniteQuery } from 'react-query'
-import { List, Message, Loader, Button } from 'semantic-ui-react'
+import { List, Message, Loader } from 'semantic-ui-react'
 import EncryptedImage from './EncryptedImage'
 import { getHomePosts } from './RoutesAuthenticated'
 import PostHeader from './PostHeader'
 import InternalLink from './Components/InternalLink'
+import InfiniteScroll from 'react-infinite-scroller'
 
 export default function () {
     const posts = useInfiniteQuery('posts', getHomePosts, {
@@ -15,26 +16,17 @@ export default function () {
     if (posts.isError) return <Message negative content='Error fetching posts' />
     if (posts.isLoading) return <Loader active />
     return (
-        <div>
-            {posts.data!.length === 0 ? (
-                <Message>To post a photo or follow people, use the menu ➚</Message>
-            ) : (
-                <>
-                    <List>
-                        {posts.data!.map((group) =>
-                            group.map((post) => (
-                                <List.Item key={post.id}>
-                                    <PostHeader post={post} />
-                                    <InternalLink to={`/post/${post.id.toString()}`}>
-                                        <EncryptedImage post={post} />
-                                    </InternalLink>
-                                </List.Item>
-                            )),
-                        )}
-                    </List>
-                    <Button disabled={!posts.canFetchMore} onClick={() => posts.fetchMore()} content='Load more' />
-                </>
+        <InfiniteScroll loadMore={() => posts.fetchMore()} hasMore={posts.canFetchMore} loader={<Loader />}>
+            {posts.data!.map((group) =>
+                group.map((post) => (
+                    <List.Item key={post.id}>
+                        <PostHeader post={post} />
+                        <InternalLink to={`/post/${post.id.toString()}`}>
+                            <EncryptedImage post={post} />
+                        </InternalLink>
+                    </List.Item>
+                )),
             )}
-        </div>
+        </InfiniteScroll>
     )
 }
