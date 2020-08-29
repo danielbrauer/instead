62b24d614572a015b1f052549593fe3ba67f9add
Daniel Brauer <messagetodaniel@gmail.com>
2020-08-29 19:15:48 +0200

    Make userProfile query stale immediately

--- client/src/UserInList.tsx
+++ client/src/UserInList.tsx
@@ -3,10 +3,9 @@ import { useQuery } from 'react-query'
 import { Icon, Label } from 'semantic-ui-react'
 import InternalLink from './Components/InternalLink'
 import { getProfile } from './postCrypto'
-import { longLivedQuery } from './QuerySettings'
 
 export default function UserInList({ id }: { id: number }) {
-    const user = useQuery(['userProfile', id], getProfile, longLivedQuery)
+    const user = useQuery(['userProfile', id], getProfile)
     return (
         <>
             <Icon size='big' name='user' />
