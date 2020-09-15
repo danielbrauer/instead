4dbbe3607e5a7618acb1d5a5227f500cab4a2d8e
Daniel Brauer <messagetodaniel@gmail.com>
2020-09-15 22:51:10 +0200

    dayjs instead of moment
    All the cool kids are using it because it's smaller

--- client/src/SingleComment.tsx
+++ client/src/SingleComment.tsx
@@ -1,15 +1,18 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import React from 'react'
-import * as Types from '../../backend/src/types/api'
 import { Comment } from 'semantic-ui-react'
-import moment from 'moment'
+import * as Types from '../../backend/src/types/api'
 import UserInList from './UserInList'
+
+dayjs.extend(relativeTime)
 
 export default function ({ comment }: { comment: Types.Comment }) {
     return (
         <Comment>
             <Comment.Content>
                 <UserInList id={comment.authorId} />
-                <Comment.Metadata content={moment(comment.published).fromNow()} />
+                <Comment.Metadata content={dayjs(comment.published).fromNow()} />
                 <Comment.Text content={comment.content} />
             </Comment.Content>
         </Comment>
