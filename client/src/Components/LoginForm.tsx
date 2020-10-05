4ab5eae0f830c15b7e9fcdd333000fa336f28603
Daniel Brauer <messagetodaniel@gmail.com>
2020-10-05 19:37:10 +0200

    Clear button for saved secret key

--- client/src/Components/LoginForm.tsx
+++ client/src/Components/LoginForm.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
 import { useMutation } from 'react-query'
 import { useHistory } from 'react-router-dom'
-import { Button, Form, Header, Label, Message } from 'semantic-ui-react'
+import { Button, ButtonProps, Form, Header, Message } from 'semantic-ui-react'
 import { cancel, loginFull, loginWithEncryptedSecretKey, UserInfo } from '../auth'
 import CurrentUser from '../CurrentUser'
 import { EncryptedSecretKey } from '../Interfaces'
 import InternalLink from './InternalLink'
+import './LoginForm.css'
 import { useInput } from './useInput'
 
 export default function LoginForm() {
     const history = useHistory()
-    const [encryptedSecretKey] = useState<EncryptedSecretKey>(CurrentUser.getEncryptedSecretKey())
+    const [encryptedSecretKey, setEncryptedSecretKey] = useState<EncryptedSecretKey | undefined>(
+        CurrentUser.getEncryptedSecretKey(),
+    )
     const { value: username, bind: bindUsername } = useInput('')
     const { value: password, bind: bindPassword, reset: resetPassword } = useInput('')
     const { value: secretKey, bind: bindSecretKey } = useInput(CurrentUser.getOldSecretKey() || '')
@@ -20,8 +23,8 @@ export default function LoginForm() {
     const [cancelMutation] = useMutation(cancel)
 
     async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
-        loginFullQuery.reset()
         evt.preventDefault()
+        loginFullQuery.reset()
         try {
             let userInfo: UserInfo | undefined
             if (encryptedSecretKey) {
@@ -37,8 +40,14 @@ export default function LoginForm() {
         }
     }
 
+    function clearSecretKey(evt: React.MouseEvent<HTMLButtonElement>, data: ButtonProps) {
+        evt.preventDefault()
+        CurrentUser.clearSecretKey()
+        setEncryptedSecretKey(undefined)
+    }
+
     return (
-        <div>
+        <>
             <Form
                 className='attached inverted segment'
                 size='large'
@@ -70,7 +79,17 @@ export default function LoginForm() {
                     {...bindPassword}
                 />
                 {encryptedSecretKey ? (
-                    <Label>{encryptedSecretKey.prefix}</Label>
+                    <div className='saved-secret-key-container'>
+                        <Button.Group fluid size='large'>
+                            <Button
+                                className='saved-secret-key-field'
+                                icon='key'
+                                disabled
+                                content={`${encryptedSecretKey.prefix}•••••••••••••••`}
+                            />
+                            <Button icon='delete' onClick={clearSecretKey} />
+                        </Button.Group>
+                    </div>
                 ) : (
                     <Form.Input fluid icon='key' iconPosition='left' placeholder='Secret Key' {...bindSecretKey} />
                 )}
@@ -80,6 +99,6 @@ export default function LoginForm() {
             <Message attached='bottom' warning>
                 New to Instead?&nbsp;<InternalLink to='/signup'>Sign up</InternalLink>&nbsp;here.
             </Message>
-        </div>
+        </>
     )
 }
